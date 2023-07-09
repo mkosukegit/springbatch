4995ed972517f2d39b6c05027450603fdb7843c4
mkosukegit <kmgit07@gmail.com>
2023-07-09 15:36:31 +0900

    パラメータ追加

--- src/main/java/com/batchsample/batchsample/tasklet/step/HelloTasklet.java
+++ src/main/java/com/batchsample/batchsample/tasklet/step/HelloTasklet.java
@@ -6,14 +6,19 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 @StepScope
 public class HelloTasklet implements Tasklet {
 
+    @Value("#{jobParameters[param1]}")
+    private String param1;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         System.out.println("Hello Task");
+        System.out.println("param1: " + param1);
         return RepeatStatus.FINISHED;
     }
 }
